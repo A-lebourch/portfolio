83e8a2c03008bc67d2aef6f1150cd9c22c1076ec
Laxenarde <alexandre.lebourch@ynov.com>
2025-03-28 00:35:27 +0100

    mise en place du site (page d'accueil + liens perso + build

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,3 @@
 <template lang="pug">
-  button(class="bg-red h-10 w-10") coucou
-  nav
-    router-link(to="/") Accueil
-    router-link(to="/about") À propos
   router-view
 </template>
-
-<style>
-nav {
-  display: flex;
-  gap: 10px;
-  margin-bottom: 20px;
-}
-</style>
