614cd1a07a663fd39a1304f17d8acc136584a747
Laxenarde <alexandre.lebourch@ynov.com>
2025-03-28 15:19:47 +0100

    ajout icones + variable globales de couleur + build + rm comments

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import './style.css'
 import App from './App.vue'
 
 import router from './router'
+import './assets/styles/variables.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const app = createApp(App)
 app.use(router)
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,18 @@
 <template lang="pug">
-  H2
-    a(href="https://www.linkedin.com/in/alexandre-le-bourch-945763203/") Mon LinkedIn
-  H2
-    a(href="https://github.com/A-lebourch/portfolio") Mon GitHub
 </template>
-  
+
+<script>
+export default {
+  name: "Template",
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted(){},
+  methods: {},
+};
+</script> 
+
+<style>
+</style>
