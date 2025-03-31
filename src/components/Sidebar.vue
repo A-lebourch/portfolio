fcff27a814dc21ca83f60e59535c7fe33b9dcbd3
Laxenarde <alexandre.lebourch@ynov.com>
2025-03-31 22:48:46 +0100

    ajout extention sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,11 +1,20 @@
 <template lang="pug">
-  div(class="sidebar")
+  div(:class="expanded ? 'sidebar-open':'sidebar-closed'")
     div(class="container")
       img(src="../assets/Logo_ALB.png" alt="Logo_ALB.png" class="image")
-    div(v-for="page in items" :key="page.path" class="sidebar-item")
-      router-link(:to="page['path']")
-        button(class="button" :style="this.$route.path == page['path'] ? 'background-color: var(--link-color)' : ''") 
-          h3 {{ page['name'] }}
+    button(@click="expanded=!expanded") test
+    div(v-if="expanded")
+      div(v-for="page in items" :key="page.path")
+        router-link(:to="page['path']")
+          //- button(class="button" :style="this.$route.path == page['path'] ? 'background-color: var(--link-color)' : ''") 
+          div(class="item_container")
+            h1(:class="page['icon']")
+            h3 {{ page['name'] }}
+    div(v-else)
+      div(v-for="page in items" :key="page.path" class="sidebar-item")
+        router-link(:to="page['path']")
+          div(class="item_container")
+            h1(:class="page['icon']")
     div(class="container")
       slot
 </template>
@@ -17,7 +26,9 @@ export default {
     items: { type: Array, required: true },
   },
   data() {
-    return {};
+    return {
+      expanded:false, 
+    };
   },
   computed: {},
   mounted() {},
@@ -26,32 +37,50 @@ export default {
 </script>
 
 <style>
+.item_container{
+  display: flex;
+  flex-direction: row;
+  overflow: visible;
+  justify-content: center;
+}
 .container {
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
 }
 
 .image {
-  width: 50%;
+  position: sticky;
+  top: 0px;
+  left: 0px;
+  width: 5rem;
   height: auto;
 }
 
-.sidebar {
+.sidebar-open {
   display: flex;
   flex-direction: column;
   background-color: var(--gray);
-  width: 10rem;
+  width: 12rem;
   height: 90vh;
   padding: 1rem;
   margin-top: 2.5vh;
   margin-left: 1.5rem;
+  transition: 0.5s;
   border-radius: 1rem;
 }
 
-.sidebar-item {
-  margin: 10px 0;
+.sidebar-closed {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--gray);
+  width: 5rem;
+  height: 90vh;
+  padding: 1rem;
+  margin-top: 2.5vh;
+  margin-left: 1.5rem;
+  transition: 0.5s;
+  border-radius: 1rem;
 }
 
 .button {
