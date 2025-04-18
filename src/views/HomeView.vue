76d770e47a094fdfa59fb4e23f9c20fc599ff02c
Laxenarde <alexandre.lebourch@ynov.com>
2025-04-18 23:35:22 +0100

    test download contact

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,5 +5,22 @@
     h3 de réaliser durant mes études et dans mon temps perso
     p _________________________________________________________
     H1 Développement en cours !
+    a(href="contact.vcf" download)
+      button.download-contact Télécharger mon contact
+
 </template>
-  
+
+<style>
+.download-contact {
+  padding: 0.5rem 1rem;
+  background-color: var(--link-color);
+  color: white;
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  font-weight: bold;
+}
+.download-contact:hover {
+  background-color: var(--button-color);
+}
+</style>
