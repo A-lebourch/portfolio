d11f4f6b9c1de39c5150e11caae7ba83b5a93e9f
Laxenarde <alexandre.lebourch@ynov.com>
2025-09-22 15:11:37 +0200

    Update deploy script permissions and comment out branch deletion command

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git commit -m "deploy update"
 git subtree push --prefix dist origin gh-pages
 
 # si pb avec la branch gh-pages on la supprime 
-git push origin --delete gh-pages
+# git push origin --delete gh-pages
